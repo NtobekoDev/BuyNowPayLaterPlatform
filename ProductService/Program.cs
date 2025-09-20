a878b4e7957866adfcd78e69a142dd5b33201c33
Ntobeko <ngwenya.ntobeko@gmail.com>
2025-09-20 22:44:43 +0200

    Migrate ProductService to SQL Server with EF Core

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -1,13 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using ProductService.Data;
 using ProductService.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ProductDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ProductDbConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddSingleton<ProductManager>();
+builder.Services.AddScoped<ProductManager>();
 builder.Services.AddEndpointsApiExplorer();
 var app = builder.Build();
 
--- ProductService/Services/ProductManager.cs
+++ ProductService/Services/ProductManager.cs
@@ -1,26 +1,41 @@
 ﻿using ProductService.Models;
+using ProductService.Data;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using System;
 
 namespace ProductService.Services
 {
-public class ProductManager
-{
-	private readonly List<Product> _products = new();
-	
-public IEnumerable<Product> GetAll() => _products;
+    public class ProductManager
+    {
+        private readonly ProductDbContext _context;
+        public ProductManager(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Product> GetAll() => _context.Products;
 
         public Product Create(string name, decimal price, bool isBNPLEligible)
-	{
-		var product = new Product
-		{
-			Id = Guid.NewGuid(),
-			Name = name,
-			Price = price,
-			isBNPLEligible = isBNPLEligible
+        {
+            var product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Price = price,
+                isBNPLEligible = isBNPLEligible
+            };
+            _context.Products.Add(product);
+            _context.SaveChanges();
 
-		};
-		_products.Add(product);
-		return product;
-	}
-}
+            return product; // Return the created product, not the DbSet
+        }
+
+        public Product GetProductById(Guid id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            return product;
+        }
+    }
 }
